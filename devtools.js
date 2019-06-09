5dc756bdbbea6fc8e6e062b9b18b1f218833796e
toby.doig <github@tobyandzuzka.com>
2019-06-09 11:42:13 +0100

    draws a graph but not quite correctly

--- devtools.js
+++ devtools.js
@@ -2,23 +2,27 @@
 
 console.log('devtools.js loaded');
 
-var backgroundPageConnection = chrome.runtime.connect({
-    name: "devtools-page"
-});
+window.addEventListener('load', () => {
+  if (chrome.devtools.inspectedWindow.tabId) {
+    var backgroundPageConnection = chrome.runtime.connect({
+        name: "devtools-page"
+    });
 
-backgroundPageConnection.onMessage.addListener(function onMessage(message) {
-  console.log('[devtools] from backgroundPageConnection says ' + JSON.stringify(message));
-});
+    backgroundPageConnection.onMessage.addListener(function onMessage(message) {
+      console.log('[devtools] from backgroundPageConnection says ' + JSON.stringify(message));
+    });
 
-backgroundPageConnection.postMessage({
-  event: 'devtools-tab-id',
-  tabId: chrome.devtools.inspectedWindow.tabId
-});
+    backgroundPageConnection.postMessage({
+      event: 'devtools-tab-id',
+      tabId: chrome.devtools.inspectedWindow.tabId
+    });
 
-chrome.devtools.panels.create("TagWeight",
-  null,
-  "panel.html",
-  function create(panel) {
-    
+    chrome.devtools.panels.create("TagWeight",
+      null,
+      "panel.html",
+      function create(panel) {
+        
+      }
+    );
   }
-);
+}, false);
--- panel.js
+++ panel.js
@@ -2,94 +2,143 @@
 
 console.log('panel.js loaded');
 
-var backgroundPageConnection = chrome.runtime.connect({
-    name: "devtools-panel"
-});
+window.addEventListener('load', () => {
+  var cy = cytoscape({
+    container: document.getElementById('cygraph'),
 
-function addMessage(m) {
-  var li = document.createElement('li');
-  li.appendChild(document.createTextNode(m));
-  
-  document.querySelector('#stuff').appendChild(li);
-}
-
-function addResource(url, parent) {
-  var p = document.getElementById(parent);
-  if (p) {
-    var u = p.querySelector('ul');
-    if (u) {
-      p = u;
-    } else {
-      u = document.createElement('ul');
-      p.appendChild(u);
-      p = u;
-    }
-  } else {
-    p = document.querySelector('#stuff');
-  }
-  
-  var li = document.createElement('li');
-  li.setAttribute('id', url);
-  li.appendChild(document.createTextNode(url));
-  
-  p.appendChild(li);
-}
-
-function findRootOfStacktrace(trace) {
-  while (trace.parent) {
-    trace = trace.parent;
-  }
-  
-  return trace;
-}
-
-function handleRequest(method, params) {
-  switch (method) {
-    case 'Network.requestWillBeSent'
-      switch (params.initiator.type) {
-        case 'parser':
-          addResource(params.request.url, params.initiator.url);
-          break;
-        
-        case 'script':
-          addResource(params.request.url, findRootOfStacktrace(params.initiator.stack).callFrames[0].url);
-          break;
-        
-        default:
-          break;
+    elements: [
+      {
+        data: { id: 'root' }
       }
-      break;
-      
-    case 'Network.responseReceived':
-      break;
-      
-    default:
-      break;
-  }
-}
+    ],
 
-backgroundPageConnection.onMessage.addListener(function onMessage(message) {
-  switch (typeof message) {
-    case 'object':
-      handleRequest(message.method, message.params);
-      break;
+    style: [
+      {
+        selector: 'node',
+        style: {
+          'background-color': '#666',
+          'label': 'data(id)'
+        }
+      },
+
+      {
+        selector: 'edge',
+        style: {
+          'width': 3,
+          'line-color': '#ccc',
+          'target-arrow-color': '#ccc',
+          'target-arrow-shape': 'triangle'
+        }
+      }
+    ]
+  });
+
+  var backgroundPageConnection = chrome.runtime.connect({
+      name: "devtools-panel"
+  });
+
+  function addMessage(m) {
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(m));
     
-    case 'array':
-      message.forEach((m) => {
-        handleRequest(m.method, m.params);
-      });
-      break;
-    
-    default:
-      document.querySelector('#stuff').innerHTML = '';
-      addMessage('reset');
-      break;
+    document.querySelector('#stuff').appendChild(li);
   }
-});
 
-backgroundPageConnection.postMessage({
-  event: 'panel-tab-id',
-  tabId: chrome.devtools.inspectedWindow.tabId
-});
+  function addToCy(url, parent) {
+    var p = cy.$id(parent) || cy.$id('root');
+    
+    try {
+      cy.add([
+        { data: { id: url } },
+        { data: { source: p.id(), target: url } }
+      ]);
+      
+      cy.layout({
+        name: 'grid'
+      }).run();
+    } catch (x) {
+      addMessage('x=' + x + ', p.id=' + p.id());
+    }
+  }
 
-addMessage('Hello');
+  function addResource(url, parent) {
+    var p = document.getElementById(parent);
+    if (p) {
+      var u = p.querySelector('ul');
+      if (u) {
+        p = u;
+      } else {
+        u = document.createElement('ul');
+        p.appendChild(u);
+        p = u;
+      }
+    } else {
+      p = document.querySelector('#stuff');
+    }
+    
+    var li = document.createElement('li');
+    li.setAttribute('id', url);
+    li.appendChild(document.createTextNode(url));
+    
+    p.appendChild(li);
+  }
+
+  function findRootOfStacktrace(trace) {
+    while (trace.parent) {
+      trace = trace.parent;
+    }
+    
+    return trace;
+  }
+
+  function handleRequest(method, params) {
+    switch (method) {
+      case 'Network.requestWillBeSent':
+        switch (params.initiator.type) {
+          case 'parser':
+            addToCy(params.request.url, params.initiator.url);
+            break;
+          
+          case 'script':
+            addToCy(params.request.url, findRootOfStacktrace(params.initiator.stack).callFrames[0].url);
+            break;
+          
+          default:
+            break;
+        }
+        break;
+        
+      case 'Network.responseReceived':
+        break;
+        
+      default:
+        break;
+    }
+  }
+
+  backgroundPageConnection.onMessage.addListener(function onMessage(message) {
+    switch (typeof message) {
+      case 'object':
+        handleRequest(message.method, message.params);
+        break;
+      
+      case 'array':
+        message.forEach((m) => {
+          handleRequest(m.method, m.params);
+        });
+        break;
+      
+      default:
+        document.querySelector('#stuff').innerHTML = '';
+        addMessage('reset');
+        break;
+    }
+  });
+
+  backgroundPageConnection.postMessage({
+    event: 'panel-tab-id',
+    tabId: chrome.devtools.inspectedWindow.tabId
+  });
+
+  addMessage('Hello');
+}, false);
